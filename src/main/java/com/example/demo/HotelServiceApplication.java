a3d9750ed791262e2e8c87f35cbad65851868419
Prinze88 <batuck420@gmail.com>
2021-04-18 00:49:49 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/HotelServiceApplication.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.Hotel;
+import com.example.demo.repos.HotelRepository;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class HotelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceApplication.class, args);
+	}
+
+	
+	@Bean
+	public CommandLineRunner runner() {
+		return new CommandLineRunner() {
+			
+			@Autowired
+			HotelRepository hotelRepository;
+			
+			@Override
+			public void run(String... args) throws Exception {
+				hotelRepository.save(new Hotel(101,"Sheraton","Chennai"));
+				hotelRepository.save(new Hotel(102,"Park Tower","Pune"));
+				hotelRepository.save(new Hotel(103,"Orange","Bengaluru"));
+				
+			}
+		};
+	}
+}
